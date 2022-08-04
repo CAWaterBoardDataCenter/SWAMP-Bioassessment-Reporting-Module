346b093085cc93543e1f28e754eca4218f4a66c4
Holder <Anna.Holder@Waterboards.ca.gov>
2022-08-04 16:06:02 -0700

    Start of retrieve data script

---
+++ scripts/00_RetrieveData.R
@@ -0,0 +1,15 @@
+# Script to retrieve Data from the California Open Data Portal
+
+# The datasets that will be included in the Reporting Module include: 
+  # - sampling locations (https://data.ca.gov/dataset/surface-water-sampling-location-information)
+  # - habitat results (https://data.ca.gov/dataset/surface-water-habitat-results)
+  # - taxonomy data (https://data.ca.gov/dataset/surface-water-benthic-macroinvertebrate-results)
+
+
+# Libraries ---------------------------------------------------------------
+
+#Install Packages
+install.packages("ckanr")
+
+# Load Packages
+library("ckanr")
